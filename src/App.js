3a81c7ab253bb735bc71a4e33e1d70e16f35c421
jsavone <john.savone@gmail.com>
2018-06-19 13:01:59 -0700

    First commit for shopping cart exercise

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import CartHeader from './Components/CartHeader'
+import CartFooter from './Components/CartFooter'
+import CartItems from './Components/CartItems'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CartHeader />
+        <CartItems />
+        <CartFooter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/CartItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cartItems = () => {
+  return (
+    <div className="container">
+      <h1>Items</h1>
+    </div>
+  )
+}
+
+export default cartItems
